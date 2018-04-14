52aaa33673092ab34b6c737c2926ef5752a764ba
vednoc <vednoc@gmail.com>
2018-04-14 21:12:12 +0200

    Added base Vue template and removed old stuff

---
+++ Frontend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Typing World!',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
